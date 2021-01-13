f33134abf5e1c4c1cce79b9407ecfb721efdb071
Evrim OĞUZ <evrimoguz@gmail.com>
2021-01-13 00:05:24 +0000

    Update optimize.sh

--- optimize.sh
+++ optimize.sh
@@ -5,10 +5,10 @@ do
     cd $d
     #show active drectory - it's not mandatory. But I like to see directory name which is processing
     pwd
-    #optimize images 
-    jpegoptim --max=10 .jpg
     #resize images
     convert *.jpg -resize 300x300 *.jpg 
+    #optimize images 
+    jpegoptim --max=10 .jpg
     #go back 
     cd ..
 done 
